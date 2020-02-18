b7b5ec1d9ed5418d0b85f55cd0f94cae0c8324ae
G4RDS <account@g4rds.jp>
2020-02-18 10:49:41 +0900

    [update] ボタンを中央に表示

--- no1/src/pages/TopPage.js
+++ no1/src/pages/TopPage.js
@@ -1,8 +1,15 @@
 import React from 'react'
+
 import './TopPage.css'
 
+import Button from '../components/Button'
+
 function TopPage() {
-  return <div></div>
+  return (
+    <div id="container">
+      <Button text="ボタン" color="#3b7efa" />
+    </div>
+  )
 }
 
 export default TopPage
