a6f4a47a592767f9d8d4256012a2a5f21cb8622d
G4RDS <account@g4rds.jp>
2020-02-21 18:16:21 +0900

    [add]] 練習用コンポーネントを追加

---
+++ no1/src/components/Practice.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Practice = props => {
+  return (
+    <div>
+      {/* (1) h2で文字を表示してみよう */}
+      <h2></h2>
+
+      {/* (3) ボタンが押されたらコンポーネントから渡された関数を呼んで伝えよう */}
+      <button>練習ボタン</button>
+    </div>
+  )
+}
+
+export default Practice
--- no1/src/pages/TopPage.js
+++ no1/src/pages/TopPage.js
@@ -4,12 +4,20 @@ import './TopPage.css'
 
 import Title from '../components/Title'
 import Button from '../components/Button'
+import Practice from '../components/Practice'
 
 function TopPage() {
+  const onPracticeClicked = () => {
+    console.log('練習ボタンがクリックされました。')
+  }
+
   return (
     <div id="container">
       <Title />
       <Button text="ボタン" color="#3b7efa" />
+
+      {/* (2) ボタンが押されたときに実行したい onPracticeClicked 関数をコンポーネントに渡そう */}
+      <Practice />
     </div>
   )
 }
