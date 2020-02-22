0ad7269700eec04e70e96ac3f32764b1d28fe276
G4RDS <account@g4rds.jp>
2020-02-22 19:37:08 +0900

    [add] 練習カウンターを追加

---
+++ no2/src/components/Practice.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+import './Practice.css'
+
+const Practice = props => {
+  // (1) useStateでステートを生成
+
+  const incrementCount = () => {
+    // (3) ステート更新関数を実行し、ステートの値を１増やす
+  }
+
+  return (
+    <div id="counter">
+      {/* (2) ステート変数を埋め込む */}
+      <span></span>
+
+      <button onClick={incrementCount}>増やす</button>
+    </div>
+  )
+}
+
+export default Practice
---
+++ no2/src/pages/TopPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './TopPage.css'
+
+import Practice from '../components/Practice'
+
+const TopPage = props => {
+  return (
+    <div id="container">
+      <Practice />
+    </div>
+  )
+}
+
+export default TopPage
