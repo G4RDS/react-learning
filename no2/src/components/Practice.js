fba931490fa474a3d65e24e3b4e9e0b55fa35923
G4RDS <account@g4rds.jp>
2020-02-22 23:47:34 +0900

    [update] 課題を追加

--- no2/src/components/Practice.js
+++ no2/src/components/Practice.js
@@ -5,6 +5,8 @@ import './Practice.css'
 const Practice = props => {
   // (1) useStateでステートを生成
 
+  // (4) useEffectで生成されたことをログに通知
+
   const incrementCount = () => {
     // (3) ステート更新関数を実行し、ステートの値を１増やす
   }
