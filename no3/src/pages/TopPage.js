84f4a125e9225bc01195c55eae8192c049b82daa
G4RDS <account@g4rds.jp>
2020-02-23 23:58:48 +0900

    [update] 解答を実装

--- no3/src/pages/TopPage.js
+++ no3/src/pages/TopPage.js
@@ -3,10 +3,20 @@ import React, { useState } from 'react'
 import './TopPage.css'
 
 function TopPage() {
-  const [name, setName] = useState('')
+  const [input, setInput] = useState({
+    name: '',
+    age: '20',
+    univ: '',
+    subject: '',
+    grade: '5',
+  })
 
-  const onNameChange = e => {
-    setName(e.target.value)
+  const onValueChange = (e, prop) => {
+    const newInput = { ...input }
+
+    newInput[prop] = e.target.value
+
+    setInput(newInput)
   }
 
   const onSubmit = e => {
@@ -14,9 +24,7 @@ function TopPage() {
     // Reactでフォームやボタン系イベントを捉えるときは、必ず実行する
     e.preventDefault()
 
-    console.log('submit', {
-      name: name,
-    })
+    console.log('submit', input)
   }
 
   return (
@@ -28,9 +36,47 @@ function TopPage() {
         <input
           className="form--input"
           type="text"
-          value={name}
-          onChange={onNameChange}
+          value={input.name}
+          onChange={e => onValueChange(e, 'name')}
         />
+
+        <h3 className="form--label">年齢</h3>
+        <input
+          className="form--input"
+          type="number"
+          value={input.age}
+          onChange={e => onValueChange(e, 'age')}
+        />
+        <span className="form--unit">才</span>
+
+        <h3 className="form--label">高専名</h3>
+        <input
+          className="form--input"
+          type="text"
+          value={input.univ}
+          onChange={e => onValueChange(e, 'univ')}
+        />
+
+        <h3 className="form--label">学科名</h3>
+        <input
+          className="form--input"
+          type="text"
+          value={input.subject}
+          onChange={e => onValueChange(e, 'subject')}
+        />
+
+        <h3 className="form--label">学年</h3>
+        <input
+          className="form--input"
+          type="text"
+          value={input.grade}
+          onChange={e => onValueChange(e, 'grade')}
+        />
+        <span className="form--unit">年</span>
+
+        <button className="form--submit" type="submit">
+          送信
+        </button>
       </form>
     </div>
   )
