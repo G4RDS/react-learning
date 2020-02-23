344824d5b5230a7485e2692f4a4f9ef0504f3f8a
G4RDS <account@g4rds.jp>
2020-02-23 23:45:25 +0900

    [update] 解答を追加

--- no4/src/pages/TopPage.js
+++ no4/src/pages/TopPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './TopPage.css'
 
@@ -22,18 +22,34 @@ const data = [
 ]
 
 function TopPage() {
+  const [search, setSearch] = useState('')
+
+  const onSearchChange = e => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div id="container">
       <div className="products">
-        <input className="products--search" type="text" placeholder="検索" />
+        <input
+          className="products--search"
+          type="text"
+          placeholder="検索"
+          value={search}
+          onChange={onSearchChange}
+        />
 
         <ul className="products--list">
-          <li className="products--item">
-            <h2 className="products--name">Kocone</h2>
-            <h3 className="products--title">
-              木更津高専生専用　個人授業マッチングアプリ
-            </h3>
-          </li>
+          {data
+            .filter(
+              p => p.name.toLowerCase().indexOf(search.toLowerCase()) >= 0
+            )
+            .map(p => (
+              <li className="products--item" key={p.name}>
+                <h2 className="products--name">{p.name}</h2>
+                <h3 className="products--title">{p.title}</h3>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
